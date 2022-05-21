deb0d07fcbd6914cf4eb90f89b0df7f7c74e0010
Veronika Sesay <veronika.sesay@gmail.com>
2022-05-21 16:56:47 +0200

    JourneyDetail a BusStop

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useState } from 'react';
 import { JourneyPicker } from '../JourneyPicker';
+import { JourneyDetail } from '../JourneyDetail';
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
   const handleJourneyChange = (journey) => {
     setJourney(journey);
   };
+  console.log(journey);
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
-      {journey != null ? `Nalezeno spojení s id ${journey.journeyId}` : null}
+      {journey !== null ? <JourneyDetail journey={journey} /> : null}
     </main>
   );
 };
