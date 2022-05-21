70780d2f6a528c44a1056b30376a5b3b00ff47d7
Veronika Sesay <veronika.sesay@gmail.com>
2022-05-21 10:45:55 +0200

    Router a Komponenta Reservation

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => (
   <>
     <Header />
     <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="reservation" element={<Reservation />} />
+      </Routes>
+    </BrowserRouter>
     <Footer />
   </>
 );
